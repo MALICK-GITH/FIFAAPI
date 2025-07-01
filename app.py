f9af3366427aa6adca3c3d467096ea30a1da0608
SOLITAIRE HACK <KINGSADDES@GMAIL.COM>
2025-07-01 16:02:54 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 import requests
 
@@ -52,4 +53,5 @@ def predict():
     return jsonify({"prediction": None})
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True) 
