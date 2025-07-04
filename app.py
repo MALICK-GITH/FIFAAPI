d7ee1e2f98ba17d900d8f972e75a193c29f4d02e
SOLITAIRE HACK <KINGSADDES@GMAIL.COM>
2025-07-04 12:25:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from predict import predict_position, enregistrer_resultat
+import os
 
 app = Flask(__name__)
 
@@ -9,10 +10,14 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    billes = int(request.form['billes'])
-    position = predict_position(billes)
-    enregistrer_resultat(billes, position)
-    return jsonify({'prediction': position})
+    try:
+        billes = int(request.form['billes'])
+        position = predict_position(billes)
+        enregistrer_resultat(billes, position)
+        return jsonify({'prediction': position})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render utilise cette variable automatiquement
+    app.run(host='0.0.0.0', port=port)
