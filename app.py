3a1b6c5033b994d37b066b3c599f12a828ca7b80
SOLITAIRE HACK <KINGSADDES@GMAIL.COM>
2025-07-04 12:31:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Render utilise cette variable automatiquement
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
