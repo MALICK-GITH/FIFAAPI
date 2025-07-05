d31b10570b895f6b3aab5e00b8b86c8645a863e7
SOLITAIRE HACK <KINGSADDES@GMAIL.COM>
2025-07-06 01:31:55 +0200

    Update predictor.py

--- services/predictor.py
+++ services/predictor.py
@@ -2,4 +2,10 @@ def predict_best_odds(odds_list):
     if not odds_list:
         return None
     sorted_odds = sorted(odds_list, key=lambda o: o.get("C", 0), reverse=True)
-    return sorted_odds[0]  # meilleure cote
+    best = sorted_odds[0]
+    return {
+        "type": best.get("T"),
+        "group": best.get("G"),
+        "value": best.get("C"),
+        "handicap": best.get("P", None)
+    }
